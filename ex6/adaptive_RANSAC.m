7e4f7c71e561b03aab853cb36473c9e3b0f1d242
Adam Kosiorek <kosiorek.adam@gmail.com>
2015-12-09 22:20:18 +0100

    finished ransac homework

---
+++ ex6/adaptive_RANSAC.m
@@ -0,0 +1,80 @@
+function [final_H, p1, p2] = adaptive_RANSAC(points1, points2, t)
+   
+    num_points = size(points1, 1);
+    assert(num_points == size(points2, 1))
+    
+    points1 = padarray(points1, [0 1], 1, 'post');
+    points2 = padarray(points2, [0 1], 1, 'post');
+    
+    N = realmax;
+    sample_size = 4;
+    
+    
+    
+    
+    best_consensus = [];
+    size_consensus = 0;
+    while N > sample_size
+        
+       % a) randomly select a sample from data and instansiate model
+       sample_index = randsample(num_points, sample_size);
+       
+       sample1 = points1(sample_index, :);
+       sample2 = points2(sample_index, :);
+       
+       H = DLT(sample1, sample2);
+       
+       % b) determine the set of data points that are within a distance threshold t of the
+       % model
+       
+       
+       dist = distance(points1, points2, H);
+       mean(sqrt(dist))
+       consensus_set = dist < t;
+       num_inliers = sum(consensus_set);
+       
+       
+       ratio = 1 - num_inliers / num_points;
+       N = log(0.01) / log(1 - (1 - ratio)^num_inliers);
+       sample_size = sample_size + 1;
+       
+%        % c) if the number of inliers is greater than T, reestimate and terminate
+%        if num_inliers > T
+%            [final_H, p1, p2] = get_result(points1, points2, consensus_set);
+%            return
+%        end
+%  
+%        % store best consensus for e)
+       if size_consensus < num_inliers
+           best_consensus = consensus_set;
+       end
+       
+       % d) rinse and repeat
+    end
+    
+    % e) pick the biggest consensus set and reestimate the model
+    [final_H, p1, p2] = get_result(points1, points2, best_consensus);
+       
+end
+
+
+function [H, p1, p2] = get_result(p1, p2, consensus)
+    p1 = p1(consensus, :);
+    p2 = p2(consensus, :);
+    H = DLT(p1, p2);
+    
+    p1 = normalize(p1);
+    p2 = normalize(p2);
+end
+
+function dist = distance(p1, p2, H)
+   
+    p2 = normalize((H * p2')');
+    dist = p2 - p1(:, 1:2);
+    dist = sum(dist.^2, 2);
+end
+
+function p = normalize(p)
+    
+   p = p(:, 1:2) ./ repmat(p(:, 3), [1 2]); 
+end
--- ex6/ex6.m
+++ ex6/ex6.m
@@ -1,16 +1,30 @@
 
+close all, clear all, clc
 
 run('../vlfeat-0.9.20/toolbox/vl_setup');
 
 box_img = single(imread('box.pgm'))/255;
 scene_img = single(imread('scene.pgm'))/255;
 
-imshow(box_img);
-
 [matched_box, matched_scene] = find_matching_points(box_img, scene_img);
 
 figure(1)
-subplot(2, 1, 1)
+subplot(3, 1, 1)
 
 showMatchedFeatures(box_img, scene_img, matched_box, matched_scene, 'montage');
 title('all matched features')
+
+
+subplot(3, 1, 2)
+% [H, matched_box, matched_scene] = RANSAC(matched_box, matched_scene, 10, 25, 20000)
+[H, matched_box, matched_scene] = adaptive_RANSAC(matched_box, matched_scene, 10)
+showMatchedFeatures(box_img, scene_img, matched_box, matched_scene, 'montage');
+title('consensus features')
+
+
+subplot(3, 1, 3)
+transform = projective2d(H');
+scene_img = imwarp(scene_img, transform);
+imshow(scene_img)
+title('warped image')
+
